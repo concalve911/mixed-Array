879956ac2ea46e6dc37f3fc03384aad34e1ebedb
concalve911 <aleksandrgarkusha911@gmail.com>
2024-07-24 06:06:08 +0200

    changes2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 "use strict";
 function averageOfNumbers(arr) {
-  var numbers = arr.filter(function (element) {
+  const numbers = arr.filter(function (element) {
     return typeof element === "number" && !isNaN(element);
   });
   if (numbers.length === 0) {
     return 0;
   }
-  var sum = numbers.reduce(function (acc, num) {
+  const sum = numbers.reduce(function (acc, num) {
     return acc + num;
   }, 0);
 
   return sum / numbers.length;
 }
 
-var mixedArray = [1, "text", 3.5, null, 7, undefined, "another text", 4];
-var result = averageOfNumbers(mixedArray);
+const mixedArray = [1, "text", 3.5, null, 7, undefined, "another text", 4];
+const result = averageOfNumbers(mixedArray);
 console.log(result);
